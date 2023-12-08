415cd6b4d23dd1fd73bc93b685d047382e4668ec
rdbla69 <109017728+rdbla69@users.noreply.github.com>
2023-12-08 17:13:46 +0800

    Add files via upload
    not yet complete

---
+++ main.js
@@ -0,0 +1,73 @@
+// change navbar styles on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+
+
+// show/hide faq answer 
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus') {
+            icon.className = "uil uil-minus";
+        } else {
+            icon.className = "uil uil-plus";
+
+        }
+    })
+})
+
+
+
+// show/hide nav menu
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+
+// close nav menu 
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
+function run(){
+    let htmlCode = document.getElementById("html-code").value;
+    let cssCode = document.getElementById("css-code").value;
+    let jsCode = document.getElementById("js-code").value;
+    let output = document.getElementById("output");
+
+    output.contentDocument.body.innerHTML = htmlCode+"<style>"+cssCode +"</style>";
+    output.contentWindow.eval(jsCode);
+}
+function openSidebar() {
+    document.querySelector(".sidebar").style.width = "250px";
+    document.querySelector(".content").style.marginLeft = "250px";
+  }
+  
+  function closeSidebar() {
+    document.querySelector(".sidebar").style.width = "0";
+    document.querySelector(".content").style.marginLeft= "0";
+  }
+  function openSidebar() {
+    document.querySelector(".sidenav").style.width = "250px";
+    document.querySelector(".main").style.marginLeft = "250px";
+  }
+  
